316c3102c24cf354df06f8d9f091c5d152a97518
Gergő Papp <imgergopapp@gmail.com>
2019-10-12 16:18:28 +0200

    Add BasicArithmeticOperations interface

---
+++ src/main/java/com/intalion/BasicArithmeticOperations.java
@@ -0,0 +1,11 @@
+package com.intalion;
+
+public interface BasicArithmeticOperations {
+	double add(double summandOne, double summandTwo);
+
+	double substract(double minuend, double subtrahend);
+
+	double multiply(double multiplier, double multiplicand);
+
+	double divide(double dividend, double divisor);
+}
